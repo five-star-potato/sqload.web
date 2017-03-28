1d30d55bef3712fe408cdd6cd8a37b71bbf46f10
Cecil Lew <formpotato@yahoo.com>
2017-03-27 23:49:44 -0400

    .

--- app.js
+++ app.js
@@ -1,14 +1,9 @@
-
-
 var express = require('express');
 var path = require('path');
 var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
-
-var routes = require('./routes/index');
-var users = require('./routes/user');
 
 var app = express();
 
@@ -25,8 +20,10 @@ app.use(bodyParser.urlencoded({
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', routes);
-app.use('/users', users);
+app.get('/',function(req,res){
+  res.sendFile(path.join(__dirname+'/index.html'));
+  //__dirname : It will resolve to your project folder.
+});
 
 /// catch 404 and forward to error handler
 app.use(function(req, res, next) {
