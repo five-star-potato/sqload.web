b31b96521b74e6650551da876782370885a6522d
Cecil Lew <formpotato@yahoo.com>
2017-03-27 23:46:04 -0400

    removed jade

--- routes/index.js
+++ routes/index.js
@@ -6,7 +6,7 @@ var path = require('path');
 
 router.get('/', function(req, res) {
   //res.send(path.join(__dirname));
-  res.sendFile(path.join(__dirname + '../view/index.html'));
+  res.sendFile(path.join(__dirname) + '../view/index.html');
 });
 
 module.exports = router;
